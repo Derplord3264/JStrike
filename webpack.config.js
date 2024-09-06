ef384a4bda97f58b66d39a2d817149018ac30e4a
Derplord3264 <96126289+Derplord3264@users.noreply.github.com>
2024-09-06 08:04:05 -0400

    hopefully fix issues

--- webpack.config.js
+++ webpack.config.js
@@ -4,46 +4,48 @@ const fs = require('fs');
 
 const nodeModules = {};
 fs.readdirSync('node_modules').filter(function(x) {
-	return ['.bin'].indexOf(x) === -1;
+    return ['.bin'].indexOf(x) === -1;
 }).forEach(function(mod) {
-	nodeModules[mod] = 'commonjs ' + mod;
+    nodeModules[mod] = 'commonjs ' + mod;
 });
 
 module.exports = {
-	entry: {
-		main: './src/main.js',
-	},
-	output: {
-		path: path.resolve(__dirname, 'build'),
-		filename: '[name].bundle.js'
-	},
-	externals: nodeModules,
-	target: 'web', // Changed from 'node' to 'web'
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			}
-		]
-	},
-	resolve: {
-		fallback: {
-			"path": require.resolve("path-browserify"),
-			"fs": false,
-			"crypto": require.resolve("crypto-browserify"),
-			"stream": require.resolve("stream-browserify"),
-			"buffer": require.resolve("buffer/")
-		}
-	},
-	stats: {
-		colors: true
-	},
-	devtool: 'source-map',
-	watch: false
+    entry: {
+        main: './src/main.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].bundle.js'
+    },
+    externals: nodeModules,
+    target: 'web', // Changed from 'node' to 'web'
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js'],
+        fallback: {
+            "path": require.resolve("path-browserify"),
+            "fs": false,
+            "crypto": require.resolve("crypto-browserify"),
+            "stream": require.resolve("stream-browserify"),
+            "buffer": require.resolve("buffer/")
+        }
+    },
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map',
+    watch: false
 };
